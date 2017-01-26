5fa2b7d87623a81d67db2a758e960d30f8e1fc12
Matt White <mattwhite614@gmail.com>
2017-01-25 18:26:44 -0600

    action complete

--- src/containers/bookList.js
+++ src/containers/bookList.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList(){
     return this.props.books.map( (book, index) => {
       return(
-        <li key={index} className="list-group-item">{book.title}</li>
+        <li onClick={() => this.props.selectBook(book)} key={index} className="list-group-item">{book.title}</li>
       );
     });
   }
@@ -25,4 +27,12 @@ function mapStateToProps(state){
   }
 }
 
-export default connect(mapStateToProps)(BookList);
+// Any value that is returned form this function, it will be called as props
+// on the BookList container
+function mapDispatchToProps(dispatch){
+  console.log(dispatch)
+  // when selectBook is called the result is passed to all of the reducers
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducerBooks.js
+++ src/reducers/reducerBooks.js
@@ -1,8 +1,8 @@
 export default function(){
   return [
-    {title:'Javascript: The Good Parts'},
+    {title: 'You don\'t Know JS'},
     {title: 'Harry Potter'},
     {title: 'The Dark Tower'},
-    {title: 'Eloquent Ruby'}
+    {title: 'Eloquent Javascript'}
   ]
 }
